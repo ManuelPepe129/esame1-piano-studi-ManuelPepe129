5895b439afdf07c6c300d888f77578113ad622c8
Manuel Pepe <manuelpepe1995@gmail.com>
2022-06-07 18:01:03 +0200

    client side login

--- client/src/API.js
+++ client/src/API.js
@@ -26,5 +26,40 @@ async function getAllIncompatibilities() {
     }
 }
 
-const API = { getAllCourses, getAllIncompatibilities };
+/* USER login API */
+
+async function login(credentials) {
+    let response = await fetch(new URL('sessions', APIURL), {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credentials),
+    });
+    if (response.ok) {
+        const user = await response.json();
+        return user;
+    } else {
+        const errDetail = await response.json();
+        throw errDetail.message;
+    }
+}
+
+async function logout() {
+    await fetch(new URL('sessions/current', APIURL), { method: 'DELETE', credentials: 'include' });
+}
+
+async function getUserInfo() {
+    const response = await fetch(new URL('sessions/current', APIURL), { credentials: 'include' });
+    const userInfo = await response.json();
+    if (response.ok) {
+        return userInfo;
+    } else {
+        throw userInfo;  // an object with the error coming from the server
+    }
+}
+
+
+const API = { getAllCourses, getAllIncompatibilities, login, logout, getUserInfo };
 export default API;
--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { MainComponent } from './CourseComponents';
+import { LoginForm } from './LoginComponents';
 import API from './API';
 
 function App() {
   const [courses, setCourses] = useState([]);
   const [incompatibilities, setIncompatibilities] = useState([]);
+  const [loggedIn, setLoggedIn] = useState(false);  // no user is logged in when app loads
+  const [user, setUser] = useState({});
+  const [message, setMessage] = useState('');
+
 
   function handleError(err) {
     console.log(err);
@@ -22,10 +27,52 @@ function App() {
       .catch(err => handleError(err));
   }, [])
 
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        // here you have the user info, if already logged in
+        // TODO: store them somewhere and use them, if needed
+        const user = await API.getUserInfo();
+        setLoggedIn(true);
+        setUser(user);
+      } catch (err) {
+        handleError(err);
+      }
+    };
+    checkAuth();
+  }, []);
+
+  const doLogin = (credentials) => {
+    API.login(credentials)
+      .then(user => {
+        setLoggedIn(true);
+        setUser(user);
+        setMessage('');
+        // navigate('/');
+      })
+      .catch(err => {
+        setMessage(err);
+      }
+      )
+  }
+
+  const doLogout = async () => {
+    await API.logout();
+    setLoggedIn(false);
+    setUser({});
+  }
+
+
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<MainComponent courses={courses} incompatibilities={incompatibilities} />}></Route>
+        <Route path='/' element={
+          <>
+            <MainComponent courses={courses} incompatibilities={incompatibilities} />
+            {(!loggedIn) ? <LoginForm login={doLogin}></LoginForm> : false}
+          </>
+        }>
+        </Route>
       </Routes>
     </Router>
   );

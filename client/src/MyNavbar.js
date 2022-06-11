bf3600451f947dbc541d8feb849105648b3324b4
Manuel Pepe <manuelpepe1995@gmail.com>
2022-06-11 22:32:45 +0200

    update navbar colors

--- client/src/MyNavbar.js
+++ client/src/MyNavbar.js
@@ -9,13 +9,13 @@ import { LogoutButton } from './LoginComponents';
 function MyNavbar(props) {
     const navigate = useNavigate();
     return (
-        <Navbar variant="dark" bg="primary">
+        <Navbar variant="primary" bg="light">
             <Container fluid>
                 <Navbar.Brand className='title'>Study Plan</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse className="justify-content-end">
                     <Navbar.Text>
-                        {props.loggedIn ? <LogoutButton user={props.user} doLogout={props.doLogout} /> : <Button onClick={() => navigate('/login')}>Log In</Button>}
+                        {props.loggedIn ? <LogoutButton user={props.user} doLogout={props.doLogout} /> : <Button onClick={() => navigate('/login')}>Log In</Button>}{' '}
                         <PersonCircle width="25" height="25" fill="currentColor" />
                     </Navbar.Text>
                 </Navbar.Collapse>

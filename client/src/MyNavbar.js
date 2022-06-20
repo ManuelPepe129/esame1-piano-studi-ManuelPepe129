09ddc4f57d4efa926db4794435c6c877aa253644
Manuel Pepe <manuelpepe1995@gmail.com>
2022-06-20 10:54:42 +0200

    better navbar display

--- client/src/MyNavbar.js
+++ client/src/MyNavbar.js
@@ -1,15 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { Navbar, Button, Container, Nav } from 'react-bootstrap';
+import { Navbar, Button, Container } from 'react-bootstrap';
 import { PersonCircle } from 'react-bootstrap-icons';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { LogoutButton } from './LoginComponents';
 
 
 function MyNavbar(props) {
     const navigate = useNavigate();
     return (
-        <Navbar variant="primary" bg="light">
+        <Navbar variant="dark" bg="dark">
             <Container fluid>
                 <Navbar.Brand className='title'>Study Plan</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
